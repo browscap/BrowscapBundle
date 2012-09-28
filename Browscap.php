312b6088cb0e0b34322df4633e05d8054043740e
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2012-09-27 22:17:16 -0500

    updated the services to just use kernel.cache_dir and updated the browscap file with some wraper functions

--- Browscap.php
+++ Browscap.php
@@ -11,10 +11,24 @@ class Browscap extends BaseBrowscap
 {
 
     /**
+     * @var stdClass
+     */
+    private $browser;
+
+    /**
      * @return boolean
      */
     public function isIPad()
     {
+        return (bool) preg_match('/iPad/', $this->_getBrowser()->browser_name);
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isIPhone()
+    {
+        return (bool) preg_match('/iPhone/', $this->_getBrowser()->browser_name);
     }
 
     /**
@@ -22,6 +36,20 @@ class Browscap extends BaseBrowscap
      */
     public function isAndroid()
     {
+        return ($this->_getBrowser()->Platform === 'Android' || $this->_getBrowser()->Browser === 'Android');
+    }
+
+    /**
+     * Just a cache method
+     *
+     * @return stdClass
+     */
+    private function _getBrowser()
+    {
+        if (null === $this->browser) {
+            $this->browser = $this->getBrowser();
+        }
+        return $this->browser;
     }
 
 }
