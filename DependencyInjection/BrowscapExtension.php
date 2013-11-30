eef1866198e0fbb0b1c8419695eb6510d9045e59
Spomky <spomky+git@gmail.com>
2013-11-30 14:29:10 +0100

    Configuration updated

--- DependencyInjection/BrowscapExtension.php
+++ DependencyInjection/BrowscapExtension.php
@@ -7,6 +7,8 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
+use Symfony\Component\Form\Exception\InvalidConfigurationException;
+
 /**
  * @author Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
  */
@@ -23,8 +25,24 @@ class BrowscapExtension extends Extension
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
 
+        if( $config['cache_dir'] === null ) {
+            $config['cache_dir'] = $container->getParameter('kernel.cache_dir');
+        }
+
+        if( !is_dir($config['cache_dir']) ) {
+
+            $this->createCacheDirectory($config['cache_dir']);
+        }
+
         foreach ($config as $k => $v) {
             $container->setParameter('browscap.' . $k, $v);
+        }
+    }
+
+    private function createCacheDirectory($cache_dir) {
+
+        if( !mkdir( $cache_dir, 0777, true) ) {
+            throw new InvalidConfigurationException('Folder "'.$cache_dir.'" does not exist and can not be created.');
         }
     }
 }
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,13 +18,43 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('browscap');
 
+        $supportedMethods = array(
+            'URL-wrapper',
+            'socket',
+            'cURL',
+            'local',
+        );
+
         $rootNode
             ->children()
+                ->scalarNode('cache_dir')
+                    ->defaultValue(null)
+                ->end()
                 ->scalarNode('remote_ini_url')
                     ->defaultValue('http://tempdownloads.browserscap.com/stream.php?BrowsCapINI')
                 ->end()
                 ->scalarNode('remote_ver_url')
                     ->defaultValue('http://tempdownloads.browserscap.com/versions/version-date.php')
+                ->end()
+                ->scalarNode('timeout')
+                    ->defaultValue(5)
+                ->end()
+                ->scalarNode('update_interval')
+                    ->defaultValue(432000)
+                ->end()
+                ->scalarNode('error_interval')
+                    ->defaultValue(7200)
+                ->end()
+                ->booleanNode('do_auto_update')
+                    ->defaultValue(true)
+                ->end()
+                ->scalarNode('update_method')
+                    ->validate()
+                        ->ifNotInArray($supportedMethods)
+                        ->thenInvalid('The method "%s" is not supported. Please choose one of ' . json_encode($supportedMethods))
+                    ->end()
+                    ->cannotBeOverwritten()
+                    ->defaultValue('cURL')
                 ->end()
             ->end();
 
