ee48737a44392f1d4ee621e1ce94eb204d8e7983
Thomas Müller <thomas.mueller@geld.de>
2017-12-01 08:05:18 +0100

    undo directory changes

--- DependencyInjection/BrowscapExtension.php
+++ DependencyInjection/BrowscapExtension.php
@@ -20,7 +20,7 @@ class BrowscapExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../resources/config'));
         $loader->load('services.xml');
 
         if (null === $config['cache_dir']) {
